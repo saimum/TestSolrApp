833194238bb809aac4a1f3c8dbf133f6f91d7d9a
salauddin saimum <saimum720@gmail.com>
2022-11-01 19:14:53 +0600

    .

--- TestSolrApp/Controllers/SolrSearchController.cs
+++ TestSolrApp/Controllers/SolrSearchController.cs
@@ -51,20 +51,5 @@ namespace TestSolrApp.Controllers
 
             }
         }
-
-        public async Task MainAsync()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:6740");
-                var content = new FormUrlEncodedContent(new[]
-                {
-                new KeyValuePair<string, string>("", "login")
-            });
-                var result = await client.PostAsync("/api/Membership/exists", content);
-                string resultContent = await result.Content.ReadAsStringAsync();
-                Console.WriteLine(resultContent);
-            }
-        }
     }
 }
